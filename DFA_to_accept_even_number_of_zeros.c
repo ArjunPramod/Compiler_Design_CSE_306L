68a5c0770592438c35005fe99ef83062b278cf53
Arjun Pramod <118660983+ArjunPramod@users.noreply.github.com>
2023-08-10 12:33:07 +0530

    Add files via upload

---
+++ DFA_to_accept_even_number_of_zeros.c
@@ -0,0 +1,56 @@
+// DFA to accept even number of zero's
+
+#include <stdio.h>
+#include <stdlib.h>
+#define max 100
+
+int main() {
+    FILE *sourceFile, *destinationFile;
+    char str[max], f = 'a';
+    int i;
+
+    sourceFile = fopen("source.txt", "r");
+    if (sourceFile == NULL) {
+        perror("Error opening source file");
+        return 1;
+    }
+
+    destinationFile = fopen("destination.txt", "w");
+    if (destinationFile == NULL) {
+        perror("Error opening destination file");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    fscanf(sourceFile, "%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        switch (f) {
+            case 'a':
+                if (str[i] == '0')
+                    f = 'b';
+                else if (str[i] == '1')
+                    f = 'a';
+                break;
+
+            case 'b':
+                if (str[i] == '0')
+                    f = 'a';
+                else if (str[i] == '1')
+                    f = 'b';
+                break;
+        }
+    }
+
+    if (f == 'a')
+        fprintf(destinationFile, "String is accepted\n");
+    else
+        fprintf(destinationFile, "String is not accepted\n");
+
+    fclose(sourceFile);
+    fclose(destinationFile);
+
+    printf("Result saved in destination_file.txt.\n");
+
+    return 0;
+}
---
+++ DFA_to_accept_string_ending_with_01.c
@@ -0,0 +1,63 @@
+// DFA to accept string ending with 01
+
+#include <stdio.h>
+#include <stdlib.h>
+#define max 100
+
+int main() {
+    FILE *sourceFile, *destinationFile;
+    char str[max], f = 'a';
+    int i;
+
+    sourceFile = fopen("source.txt", "r");
+    if (sourceFile == NULL) {
+        perror("Error opening source file");
+        return 1;
+    }
+
+    destinationFile = fopen("destination.txt", "w");
+    if (destinationFile == NULL) {
+        perror("Error opening destination file");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    fscanf(sourceFile, "%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        switch (f) {
+            case 'a':
+                if (str[i] == '0')
+                    f = 'b';
+                else if (str[i] == '1')
+                    f = 'a';
+                break;
+
+            case 'b':
+                if (str[i] == '0')
+                    f = 'b';
+                else if (str[i] == '1')
+                    f = 'c';
+                break;
+
+            case 'c':
+                if (str[i] == '0')
+                    f = 'b';
+                else if (str[i] == '1')
+                    f = 'a';
+                break;
+        }
+    }
+
+    if (f == 'c')
+        fprintf(destinationFile, "String is accepted\n");
+    else
+        fprintf(destinationFile, "String is not accepted\n");
+
+    fclose(sourceFile);
+    fclose(destinationFile);
+
+    printf("Result saved in destination_file.txt.\n");
+
+    return 0;
+}
